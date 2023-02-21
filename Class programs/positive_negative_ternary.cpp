9e57c7b696d4b7019f6afd534c8b82ca6dcf08e3
vedantBCodes <bichkundevedant97@gmail.com>
2023-02-21 10:05:23 -0800

    class program added

---
+++ Class programs/positive_negative_ternary.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    int a;
+    cout<<"enter a number:";
+    cin>>a;
+    cout<<a;
+    a>0?cout<<" is positive ":cout<<" is negative ";
+
+}
+
