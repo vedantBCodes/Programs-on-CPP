8f37bbf654a25c763343c794ad3e03349982f46e
vedantBCodes <bichkundevedant97@gmail.com>
2023-03-04 09:38:57 -0800

    New program added

---
+++ Class programs/countingPairsWithGivenSum.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class pairs
+{
+private:
+    int arr[20],num,sum,i,j;
+public:
+    input()
+    {
+        cout<<"How many numbers you want to take:";
+        cin>>num;
+        cout<<"Enter "<<num<<" numbers:";
+        for(i=0;i<num;i++)
+        {
+            cin>>arr[i];
+        }
+        cout<<"how much should be sum of 2 input array elements:";
+        cin>>sum;
+    }
+    result()
+    {
+        int cnt=0;
+        for(i=0;i<num;i++)
+        {
+            for(j=i+1;j<num;j++)
+            {
+                if(arr[i]+arr[j]==sum)
+                {
+                    cnt++;
+                }
+            }
+        }
+        cout<<"The number of pairs whose sum is "<<sum<<" are:"<<cnt;
+    }
+
+};
+int main()
+{
+    pairs obj;
+    obj.input();
+    obj.result();
+}
