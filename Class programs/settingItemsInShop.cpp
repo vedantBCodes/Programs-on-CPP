c668bf6feb0ad1ea639d425f512897a00b8697ea
vedant Yevatikar <96629752+vedantBCodes@users.noreply.github.com>
2023-07-29 18:29:55 +0530

    new program added

---
+++ Class programs/settingItemsInShop.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class shop
+{
+private:
+    int itemId[100];
+    int price[100];
+    int cnt;
+
+public:
+    void initializingCount()
+    {
+        cnt = 0;
+    }
+    void setItem();
+    void displayItem();
+};
+void shop::setItem()
+{
+    cout << "Enter the ID of an item:";
+    cin >> itemId[cnt];
+    cout << "Enter the price of that item:";
+    cin >> price[cnt];
+    cnt++;
+}
+void shop::displayItem()
+{
+    for (int i = 0; i < cnt; i++)
+    {
+        cout << "The Price of an item with id " << itemId[i] << " is " << price[i] << endl;
+    }
+}
+int main()
+{
+    shop marutiKiranaStore;
+    int x;
+    marutiKiranaStore.initializingCount(); // It will initalize the value of cnt to 0;
+    cout << "How many items you want to add in your shop:";
+    cin >> x;
+    for (int i = 0; i < x; i++)
+    {
+        marutiKiranaStore.setItem();
+    }
+    marutiKiranaStore.displayItem();
+    // We are not calling this displayItem() function in for loop because we already put the for the in the function defination of this function
+
+    return 0;
+}
