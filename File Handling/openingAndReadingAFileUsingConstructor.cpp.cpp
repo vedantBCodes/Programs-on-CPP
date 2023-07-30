81db688fcb75dd10db717f3390e4ce2340930db5
vedant Yevatikar <96629752+vedantBCodes@users.noreply.github.com>
2023-07-30 14:49:41 +0530

    Update openingAndReadingAFileUsingConstructor.cpp.cpp

--- File Handling/openingAndReadingAFileUsingConstructor.cpp.cpp
+++ File Handling/openingAndReadingAFileUsingConstructor.cpp.cpp
@@ -6,7 +6,7 @@ int main()
 {
     string str;
     ifstream in("demo.txt");
-    getline(in,str);
+    getline(in,str); //This will read only first line from the whole file and stored it in str string
     cout<<str;
     in.close();
    return 0;
