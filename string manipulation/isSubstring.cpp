90c52fa9e461b7522825418d5109c80a32509b80
vedant Yevatikar <96629752+vedantBCodes@users.noreply.github.com>
2023-08-01 10:32:09 +0530

    new program added

---
+++ string manipulation/isSubstring.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+bool isSubstringPresent(char str[],char subStr[])
+{
+    int len1=strlen(str);
+    int len2=strlen(subStr);
+    int x,cnt;
+    bool check=false;
+    for(int i=0;i<len1;i++)
+    {
+        x=0;
+        cnt=0;
+        if(str[i]==subStr[0])
+        {
+            for(int j=i;j<(i+len2);j++)
+            {
+                if(str[j]==subStr[x])
+                {
+                    cnt++;
+                    x++;
+                }
+            }
+            if(cnt==len2)
+            {
+                check=true;
+            }
+        }
+    }
+    if(check==true)
+     {
+         return true;
+     }
+    else
+     {
+         return false;
+     }
+}
+
+int main()
+{
+    char str[50];
+    char subStr[30];
+    cout<<"Enter a string:";
+    cin.getline(str,40);
+    cout<<"Enter a substring you want to search:";
+    cin.getline(subStr,20);
+    bool check=isSubstringPresent(str,subStr);
+    if(check==true)
+    {
+        cout<<subStr<<" is substring of "<<str;
+    }
+    else
+    {
+        cout<<subStr<<" is not a substring of "<<str;
+    }
+}
