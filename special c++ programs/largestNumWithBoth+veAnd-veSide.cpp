b272311db5518706da736d7f251820eed1bbdedf
vedant Yeotikar <bichkundevedant97@gmail.com>
2025-08-05 21:03:28 +0530

    added better approach

--- special c++ programs/largestNumWithBoth+veAnd-veSide.cpp
+++ special c++ programs/largestNumWithBoth+veAnd-veSide.cpp
@@ -1,44 +1,44 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 //Finding the largest number whose both +ve and -ve side is present in array
-int main()
+int largestNumber(int arr[] , int Asize )
 {
-    int arr[]={4,5,-3,3,-5,9,-7,8}; //Here 5 is the largest number with both +ve and -ve side
-    int Asize=8;
-    int i,j;
-    for(i=0;i<Asize;i++)
+    int max=INT_MIN;
+    for(int i=0;i<Asize ;i++)
     {
-        for(j=(i+1);j<Asize;j++)
+        for(int j=0;j<Asize ; j++)
         {
-            if(arr[i]<arr[j])
+            if(arr[j]==(-(arr[i])))
             {
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-            }
-        }
-    }
-    bool x=true;
-    for(i=0;i<Asize;i++)
-    {
-        if(arr[i]>0)
-        {
-        for(j=(Asize-1);j>0;j--)
-        {
-            if(arr[j]<0)
-            {
-                if((arr[i]*arr[i])==(arr[j]*arr[j]))
+              if(arr[j]>0 && arr[j]>max)
               {
-                cout<<"The largest number with both +ve and -ve sides is:"<<arr[i];
-                x=false;
+                  max= arr[j];
+              }
+            else if(arr[i]>0 && arr[i]>max)
+              {
+                  max= arr[i];
               }
             }
         }
-        }
-        if(x==false)
-            {
-               break; 
-            }
     }
-
+    if( max == INT_MIN )
+    {
+        return 1;
+    }
+        return max;
+}
+int main()
+{
+    int arr[]={4,5,-3,3,-5,7,-9,9};
+    int Asize=8;
+    int num = largestNumber(arr,Asize);
+    if(num!=1)
+    {
+        cout<<num<<" is your number";
+    }
+    else
+    {
+        cout<<" Number not found ";
+    }
 }
