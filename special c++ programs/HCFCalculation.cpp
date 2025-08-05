3f198b53e5fc6989940e5b9754f4ee875b048f72
vedant Yeotikar <bichkundevedant97@gmail.com>
2025-08-05 18:21:23 +0530

    added the best approach to calculate HCF

--- special c++ programs/HCFCalculation.cpp
+++ special c++ programs/HCFCalculation.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void   HCFCalculation(int x,int y)
+void   HCFCalculation1(int x,int y)  //Wrost approach 
 {
     int arr1[20],arr2[20],m=1,n=1,i,j,a,b;
     for(i=0;i<10;i++)
@@ -31,10 +31,24 @@ cout<<"HCF of "<<x<<" and "<<y<<" is:"<<1;
            }}
         }
 }
+
+int HCFCalculation2(int num1, int num2)   //Best Approach
+{
+    int minVal = min(num1, num2);
+    for(int i=minVal ; i>=1 ; i--)
+    {
+        if((num1 % i == 0) && (num2 % i == 0))
+        {
+           return i;
+        }
+    }
+    return 1;
+}
 int main()    //program to calculate HCF of 2 numbers
 {
     int x,y;
     cout<<"enter two numbers:";
     cin>>x>>y;
-    HCFCalculation(x,y);
+    HCFCalculation1(x,y);
+    HCFCalculation2(x,y);
 }
