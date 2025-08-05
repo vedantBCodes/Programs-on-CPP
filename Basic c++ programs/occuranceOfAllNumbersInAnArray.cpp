dd2686895bab3f0b2a887eb149ac729eeea3f07b
vedant Yeotikar <bichkundevedant97@gmail.com>
2025-08-05 12:01:34 +0530

    Update occuranceOfAllNumbersInAnArray.cpp

--- Basic c++ programs/occuranceOfAllNumbersInAnArray.cpp
+++ Basic c++ programs/occuranceOfAllNumbersInAnArray.cpp
@@ -1,37 +1,40 @@
 #include<iostream>
 using namespace std;
-int main( )
-{
+
+int main() {
     int arr1[20];
-    int arr2[20],i,j,k,num;
-    cout<<"How many numbers you want to take:";
-    cin>>num;
-    cout<<"Enter "<<num<<" array elements:";
-    for(i=0;i<num;i++)
-    {
-        cin>>arr1[i];
+    int num;
+
+    cout << "How many numbers you want to take: ";
+    cin >> num;
+
+    cout << "Enter " << num << " array elements: ";
+    for (int i = 0; i < num; i++) {
+        cin >> arr1[i];
     }
-    for(i=0;i<num;i++)
-  {
-        int cnt=0;
-        int x=0;
-        for(j=i-1;j>=0;j--)
-        {
-            if(arr1[i]!=arr1[j])
-            {
-            x++;
+
+    // Count frequency of each unique element
+    for (int i = 0; i < num; i++) {
+        bool alreadyCounted = false;
+
+        // Check if already counted
+        for (int j = 0; j < i; j++) {
+            if (arr1[i] == arr1[j]) {
+                alreadyCounted = true;
+                break;
             }
         }
-        if(x==i)
-        {
-        for(k=0;k<num;k++)
-        {
-            if(arr1[i]==arr1[k])
-            {
-                cnt++;
+
+        if (!alreadyCounted) {
+            int count = 1;
+            for (int k = i + 1; k < num; k++) {
+                if (arr1[i] == arr1[k]) {
+                    count++;
+                }
             }
-        }
-        cout<<arr1[i]<<" occurs "<<cnt<<" times"<<endl;
+            cout << arr1[i] << " occurs " << count << " time(s)" << endl;
         }
     }
+
+    return 0;
 }
