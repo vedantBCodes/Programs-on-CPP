f4db9cd7e8d7f63d9c036303d463441c7fc9432e
vedant Yeotikar <bichkundevedant97@gmail.com>
2025-08-05 12:30:20 +0530

    Update primeNumbers.cpp

--- Basic c++ programs/primeNumbers.cpp
+++ Basic c++ programs/primeNumbers.cpp
@@ -1,23 +1,25 @@
 #include<iostream>
+#include<cmath> // for sqrt
 using namespace std;
+
 int main()
 {
-    int num,cnt=0;
-    cout<<"Enter a number to check whether it is prime or not:";
-    cin>>num;
-    for(int i=2;i<num;i++)
-    {
-        if(num%i==0)
-        {
-            cnt++;
+    int num;
+    cout << "Enter a number to check whether it is prime or not: ";
+    cin >> num;
+
+    if (num <= 1) {
+        cout << num << " is not a prime number.";
+        return 0;
+    }
+
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) {
+            cout << num << " is not a prime number.";
+            return 0;
         }
     }
-    if(cnt==0)
-    {
-        cout<<num<<" is a prime number";
-    }
-    else
-    {
-        cout<<num<<" is not a prime number";
-    }
+
+    cout << num << " is a prime number.";
+    return 0;
 }
