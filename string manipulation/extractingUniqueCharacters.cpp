56bdbeb500f0f213e441f9f08d3ae9b2e81c7ca2
vedant Yeotikar <bichkundevedant97@gmail.com>
2025-08-10 12:15:28 +0530

    Update extractingUniqueCharacters.cpp

--- string manipulation/extractingUniqueCharacters.cpp
+++ string manipulation/extractingUniqueCharacters.cpp
@@ -26,6 +26,29 @@ void extractingUniqueChracters(char str[])
         }
     }
 }
+string extractingUniqueChracters2(string str) // Best approach using String
+{
+   int size = str.size();
+   string newStr ="";
+   for(int i=0;i<size;i++)
+   {
+       bool check=true;
+       for(int j=0;j<newStr.size();j++)
+       {
+           if(str[i]==newStr[j])
+           {
+               check=false;
+               break;
+           }
+       }
+       if(check==true)
+       {
+           newStr.push_back(str[i]);
+       }
+   }
+   return newStr;
+}
+
 int main()
 {
     char str[]="abcdbg";
