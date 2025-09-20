6936e4dc302959a0234cdb87cd0820ab9f69cd0d
vedant Yeotikar <bichkundevedant97@gmail.com>
2025-09-21 00:15:06 +0530

    added correct and optimal appraoch

--- Class programs/armstrong.cpp
+++ Class programs/armstrong.cpp
@@ -1,31 +1,42 @@
 #include<iostream>
 using namespace std;
-
+/* A number is called an Armstrong number if the sum of its digits each raised to the power of the number of digits equals the number itself.*/
 class Armstrong {
 private:
     int num;
 
 public:
-    void input() {
+    void input() 
+    {
         cout << "Enter a number: ";
         cin >> num;
     }
 
-    void process() {
-        int sum = 0;
-        int temp = num;
-
-        while (temp > 0) {
-            int digit = temp % 10;
-            sum += digit * digit * digit;
-            temp /= 10;
-        }
-
-        if (sum == num) {
-            cout << num << " is an Armstrong number.";
-        } else {
-            cout << num << " is not an Armstrong number.";
-        }
+    void process() 
+   {
+    int temp1 = num;
+    int cnt=0;
+    while(temp1>0)
+    {
+        cnt++;
+        temp1=temp1/10;
+    }
+    int temp2 = num;
+    int sum = 0;
+    while(temp2 > 0)
+    {
+        int digit = temp2 % 10;
+        sum = sum + pow(digit,cnt);
+        temp2=temp2/10;
+    }
+    if (sum == num) 
+    {
+        cout << num << " is an Armstrong number.";
+    }
+    else 
+    {
+        cout << num << " is not an Armstrong number.";
+    }
     }
 };
 
@@ -33,5 +44,6 @@ int main() {
     Armstrong obj;
     obj.input();
     obj.process();
+    obj.armstrong
     return 0;
 }
